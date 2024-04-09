dbf43e24bcb38afeb097c814f2d567eb382d5adb
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-04-09 01:25:06 -0500

    Update API version to 0.0.2 in main.py :D

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
     title='Globant\'s  Data  Engineering  Coding Challenge',
     description='API for uploading CSV files and saving their data in the database, as part of Globant\'s Data Engineering Coding Challenge.',
     summary='API for uploading CSV files and saving their data in the database',
-    version='0.0.1',
+    version='0.0.2',
 )
 
 app.include_router(index.router)
