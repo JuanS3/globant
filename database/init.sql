c23df58f9cf8f4726dbc29076424f233b5b6008f
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2024-04-08 22:53:32 -0500

    feat: :sparkles: Refactor database initialization script and add foreign key constraints

--- database/init.sql
+++ database/init.sql
@@ -1,12 +1,3 @@
--- Crear la tabla de empleados si no existe
-CREATE TABLE IF NOT EXISTS employees (
-    id              INTEGER PRIMARY KEY,
-    name            VARCHAR(255) NOT NULL,
-    hire_datetime   TIMESTAMP NOT NULL,
-    department_id   INTEGER NOT NULL,
-    job_id          INTEGER NOT NULL
-);
-
 -- Crear la tabla de departamentos si no existe
 CREATE TABLE IF NOT EXISTS departments (
     id          INTEGER PRIMARY KEY,
@@ -17,4 +8,13 @@ CREATE TABLE IF NOT EXISTS departments (
 CREATE TABLE IF NOT EXISTS jobs (
     id      INTEGER PRIMARY KEY,
     job     VARCHAR(255) NOT NULL
+);
+
+-- Crear la tabla de empleados si no existe
+CREATE TABLE IF NOT EXISTS employees (
+    id              INTEGER PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    hire_datetime   TIMESTAMP NOT NULL,
+    department_id   INTEGER REFERENCES departments(id) NOT NULL,
+    job_id          INTEGER REFERENCES jobs(id) NOT NULL
 );
